1137b3247fb04bc74402dfacf080b821cffcf8a3
Dash <tolofaridagogo@gmail.com>
2024-10-04 19:38:15 +0200

    update

--- DemoFunction/Demo.cs
+++ DemoFunction/Demo.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Azure.Messaging.ServiceBus;
 using DemoFunction.Models;
+using FluentEmail.Razor;
+using FluentEmail.Smtp;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
@@ -25,10 +29,60 @@ namespace DemoFunction
             ServiceBusMessageActions messageActions)
         {
            var jsonString = Encoding.UTF8.GetString(message.Body);
-            PersonModel person = JsonSerializer.Deserialize<PersonModel>(jsonString);
+            MyEmailModel email = JsonSerializer.Deserialize<MyEmailModel>(jsonString);
+
+            //send email
+            SendEmail(email.Email, email.Subject, email.HtmlMessage);
 
             // Complete the message
             await messageActions.CompleteMessageAsync(message);
+
+            
+        }
+
+        private async Task SendEmail(string email, string subject, string htmlMessage)
+        {
+            var credentials = new NetworkCredential()
+            {
+                UserName = "emailappsmtp.76f497bcd011bb11",  // replace with valid value
+                Password = "XFKuxEvmPZ4n"  // replace with valid value
+            };
+            var sender = new SmtpSender(() => new SmtpClient("smtp.zeptomail.com")
+            {
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Port = 587,
+                Credentials = credentials,
+
+                //DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory,
+                //PickupDirectoryLocation = @"C:\Demos"
+            });
+
+            StringBuilder template = new();
+            template.AppendLine(htmlMessage);
+
+            FluentEmail.Core.Email.DefaultSender = sender;
+            FluentEmail.Core.Email.DefaultRenderer = new RazorRenderer();
+
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.WriteLine(template.ToString());
+            writer.Flush();
+            stream.Position = 0;
+
+            var contentType = new System.Net.Mime.ContentType(System.Net.Mime.MediaTypeNames.Application.Pdf);
+            var reportAttachment = new Attachment(stream, contentType);
+            reportAttachment.ContentDisposition.FileName = "yourFileName.pdf";
+
+            var myEmail = FluentEmail.Core.Email
+                .From("do-not-reply@www.nddc.gov.ng", "NDDC Notification")
+                .Subject(subject)
+                .To(email)
+                .UsingTemplate(template.ToString(), new { });
+
+
+
+            var response = await myEmail.SendAsync();
         }
     }
 }
---
+++ DemoFunction/Models/MyEmailModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoFunction.Models
+{
+    public class MyEmailModel
+    {
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string HtmlMessage { get; set; }
+    }
+}
